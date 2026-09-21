caff3e6cea0a4ce08dc9b87e8beaa7dc73f1978b
asdfghjklpoiuytrewq <daiki20080214@gmail.com>
2026-09-21 22:28:18 +0900

    Update script.js

--- script.js
+++ script.js
@@ -103,6 +103,7 @@
 
                   <select class="time-select">
                     <option value="">--</option>
+                    <option>10:00</option><option>10:30</option><option>11:00</option><option>11:30</option>
                     <option>12:00</option><option>12:30</option><option>13:00</option>
                     <option>13:30</option><option>14:00</option><option>14:30</option>
                     <option>15:00</option><option>15:30</option><option>16:00</option>
